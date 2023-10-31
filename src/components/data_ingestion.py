d6e66d428392972aba7a4f7b0015ca6ffe32355b
Pritam1026 <singhpritam983@gmail.com>
2023-11-01 00:57:27 +0530

    model trainr

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,10 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
     '''This class will initiate the variables or paths required to save the data and train test split
@@ -60,4 +64,9 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
+
 
